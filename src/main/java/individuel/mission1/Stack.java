fdd9ab387f0df5b3a11f5dfc9814fafdf487d6e3
Damien Deprez <damien.deprez@student.uclouvain.be>
2015-09-27 12:22:33 +0200

    Ajout de l'interface Stack

---
+++ src/main/java/individuel/mission1/Stack.java
@@ -0,0 +1,33 @@
+package individuel.mission1;
+
+import java.util.EmptyStackException;
+
+public interface Stack <E>{
+	
+		/**
+		 * Test si la Stack est vide
+		 * @return true si la stack et vide false aussi non
+		 */
+	    public boolean empty();
+
+	    /**
+	     * Regarde l'élément au sommet de la Stack sans le retirer de la pile
+	     * @return les données de l'élément du sommet de la Stack
+	     * @throws EmptyStackException
+	     */
+	    public E peek() throws EmptyStackException;
+
+	    /**
+	     * Retire le premier élément de la Stack
+	     * @return les données du premier élément de la Stack
+	     * @throws EmptyStackException
+	     */
+	    public E pop() throws EmptyStackException;
+
+	    /**
+	     * Ajoute un élément au sommet de la Stack
+	     * @param item élément à ajouter à la Stack
+	     * @return l'élément ajouté à la Stack
+	     */
+	    public E push(E item);
+}
