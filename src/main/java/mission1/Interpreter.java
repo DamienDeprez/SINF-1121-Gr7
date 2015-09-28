910c62d193b2c36f441125312a18f115264ff8a3
miserentant <merlin.iserentant@student.uclouvain.be>
2015-09-28 17:20:38 +0200

    debut

--- src/main/java/mission1/Interpreter.java
+++ src/main/java/mission1/Interpreter.java
@@ -3,5 +3,26 @@ package mission1;
 import testmission1.InterpreterTests;
 
 public class Interpreter extends InterpreterTests{
-
+	public String interpret(String instructions){
+		String stringretour =null;
+		String[] str = instructions.split(" ");
+		MyStack<String> mystack1 = new MyStack<String>();
+		int i;
+		for(int i = 0; i < str.length; i++){
+			if(isanint(str[i])){
+				
+			}
+		}
+		
+		return stringretour;
+		
+	}
+	private boolean  isanint(String string) {
+		try {
+			Integer.parseInt(string);
+		} catch (NumberFormatException e){
+			return false;
+		}
+		return true;
+	}
 }
